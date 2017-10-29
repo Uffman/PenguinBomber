e8b5d130b3cfdb1ed3a1d4508a423be30100cb10
Marko <marko@Markos-Mac.local>
2017-10-29 18:25:31 +0100

    Initial Commit

---
+++ MyGame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGame
+//
+//  Created by Marko on 29/10/2017.
+//  Copyright © 2017 Marko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
